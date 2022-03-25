d5adf9615ed637bec8e83f190a46dd0983b962e6
liguanghao <liguanghao@viatomtech.com>
2022-03-25 17:22:08 +0800

    fxgbxdfggfh

--- lib/main.dart
+++ lib/main.dart
@@ -51,8 +51,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   final userScreen=false;
-
+  bool _inCalling = true;
   final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
   @override
   initState() {
     super.initState();
@@ -69,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
           : {
         'mandatory': {
           'minWidth':
-          '320', // Provide your own width, height and frame rate here
+          '640', // Provide your own width, height and frame rate here
           'minHeight': '640',
           'minFrameRate': '30',
         },
@@ -108,47 +109,62 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Container(
-                width:  120.0,
-                height: 90.0,
-                child: RTCVideoView(_localRenderer, mirror: true),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _inCalling
+          ? SizedBox(
+          width: 200.0,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: () {  },
+                  child: const Icon(Icons.switch_camera),
+
+                ),
+                FloatingActionButton(
+                  tooltip: 'Hangup',
+                  child: Icon(Icons.call_end),
+                  backgroundColor: Colors.pink,
+                  onPressed: () {  },
+                ),
+                FloatingActionButton(
+                  onPressed: () {  },
+                  child: const Icon(Icons.mic_off),
+
+                )
+              ]))
+          : null,
+      body: Container(
+        child: Stack(children: <Widget>[
+          Positioned(
+              left: 0.0,
+              right: 0.0,
+              top: 0.0,
+              bottom: 0.0,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: RTCVideoView(_remoteRenderer),
                 decoration: BoxDecoration(color: Colors.black54),
+              )),
+          Positioned(
+            left: 20.0,
+            top: 20.0,
+            child: Container(
+              width:  120.0,
+              height: 60,
+              child: RTCVideoView(_localRenderer, mirror: true),
+              decoration: BoxDecoration(color: Colors.black54),
             ),
-          ],
-        ),
+          ),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
